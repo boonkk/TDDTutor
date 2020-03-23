23144515a85bb038e8df61021134ad8d088ad75f
marcinolech <mrc.olech@gmail.com>
2020-03-23 17:19:47 +0100

    2

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 public class DollarTest {
 
+
     @Test
-    public void addingTest(){
-        Dollar money = new Dollar(2);
-        money.times(3);
-        Assert.assertEquals(6,money.getAmount());
-        money.add(5);
-        Assert.assertEquals(11, money.getAmount());
+    public void multiplicationTest() {
+        Dollar five= new Dollar(5);
+        Dollar product= five.times(2);
+        Assert.assertEquals(10, product.getAmount());
+        product= five.times(3);
+        Assert.assertEquals(15, product.getAmount());
     }
 }
